b9f9302e2b83391eacd28e4429e0921f838e9f5f
gopalbehere <91052141+gopalbehere@users.noreply.github.com>
2024-01-26 16:00:56 +0530

    Add files via upload

---
+++ bookAngular/src/main/java/org/anudip/bookAngular/BookAngularApplication.java
@@ -0,0 +1,13 @@
+package org.anudip.bookAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAngularApplication.class, args);
+	}
+
+}
